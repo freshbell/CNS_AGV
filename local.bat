8fed5154c3fc4e7065d0f0dae1e5db4dcccb9bf0
freshbell <68316796+freshbell@users.noreply.github.com>
2021-11-29 14:26:40 +0900

    patch

--- local.bat
+++ local.bat
@@ -1,20 +1,20 @@
 call .\venv\Scripts\activate.bat
-start python client.py AGV00001 0
+start python3 client.py AGV00001 0
 timeout 1
-start python client.py AGV00002 0
+start python3 client.py AGV00002 0
 timeout 1
-start python client.py AGV00003 0
+start python3 client.py AGV00003 0
 timeout 1
-start python client.py AGV00004 0
+start python3 client.py AGV00004 0
 timeout 1
-start python client.py AGV00005 0
+start python3 client.py AGV00005 0
 timeout 1
-start python client.py AGV00006 0
+start python3 client.py AGV00006 0
 timeout 1
-start python client.py AGV00007 0
+start python3 client.py AGV00007 0
 timeout 1
-start python client.py AGV00008 0
+start python3 client.py AGV00008 0
 timeout 1
-start python client.py AGV00009 0
+start python3 client.py AGV00009 0
 timeout 1
-start python client.py AGV00010 0
+start python3 client.py AGV00010 0
